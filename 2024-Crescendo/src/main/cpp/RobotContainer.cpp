637232defd585ff841035343ce6b55c8dbb5d910
Elitelord <73070716+Elitelord@users.noreply.github.com>
2024-01-25 22:31:50 -0600

    vision pid stuff

--- 2024-Crescendo/src/main/cpp/RobotContainer.cpp
+++ 2024-Crescendo/src/main/cpp/RobotContainer.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors. 
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -18,6 +18,7 @@
 #include "networktables/NetworkTableInstance.h"
 #include "networktables/NetworkTableEntry.h"
 #include "networktables/NetworkTableValue.h"
+#include <frc/controller/PIDController.h>
 
 using namespace std;
 using namespace frc2;
@@ -42,18 +43,26 @@ RobotContainer::RobotContainer() {
         frc::SmartDashboard::PutNumber("y",YAxis);
         frc::SmartDashboard::PutNumber("rot",RotAxis);
 
-        if(m_driverController.GetRawButton(11)){
+        if(m_driverController.GetRawButton(11)){  
           m_drive.moveToAngle(XAxis, YAxis);
         }else if(m_driverController.GetRawButton(12)){
           m_drive.moveToAngle(0, 0.3);
         }
         std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-        
-        if (m_driverController.GetRawButton(7)){
+            frc::PIDController pid(kvP, kvI, kvD);
+            
             double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
             double heading_error = targetOffsetAngle_Horizontal;
-            RotAxis += heading_error * SwerveModuleConstants::kvP*speedMultiplier;
+            // pid.SetSetpoint(0);
+            frc::SmartDashboard::PutNumber("heading", heading_error);
+            double output = pid.Calculate(heading_error, 0);   
+            // pid.Calculate();
+        if (m_driverController.GetRawButton(7)){
+            
+            
+            RotAxis += output * speedMultiplier;
         }
+        frc::SmartDashboard::PutNumber("pid", output);  
         // else {
         m_drive.swerveDrive(XAxis, YAxis, RotAxis, false);
         // }
--- 2024-Crescendo/src/main/include/Constants.h
+++ 2024-Crescendo/src/main/include/Constants.h
@@ -111,7 +111,10 @@ namespace SwerveModuleConstants {//per swerve module
     constexpr double kyD = 0.0;
     constexpr double kyFF = 0.00000;
 
-    constexpr float kvP = -0.3f;
+    constexpr float kvP = 0.05f;
+    constexpr float kvI = 0.01f;
+    constexpr float kvD = 0.0001f;
+    constexpr float kvFF = 0.0f;
     //possible heaving, x, and y PID for auto/pathplanning
 
     constexpr double kRampTimeSeconds = 0.1; // slew rate limiter (delay on acceleration)
