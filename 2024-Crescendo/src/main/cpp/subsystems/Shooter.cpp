8852a7057f606a02424f69c400f6a9018ecb00fc
tonychen5427 <tonychenjs@gmail.com>
2024-02-29 12:32:55 -0600

    test

--- 2024-Crescendo/src/main/cpp/subsystems/Shooter.cpp
+++ 2024-Crescendo/src/main/cpp/subsystems/Shooter.cpp
@@ -21,8 +21,8 @@ void Shooter::resetMotors(){
     m_leftMotor.EnableVoltageCompensation(12.0);
     m_leftMotor.SetSmartCurrentLimit(20, 40);
 
-    m_leftEncoder.SetPositionConversionFactor(((wheelDiameter*MathConstants::pi) / pulleyRatio).value());
-    m_leftEncoder.SetVelocityConversionFactor(((wheelDiameter * MathConstants::pi) / pulleyRatio / 60_s).value());
+    m_leftEncoder.SetPositionConversionFactor(((wheelDiameter*MathConstants::pi) * pulleyRatio).value());
+    m_leftEncoder.SetVelocityConversionFactor(((wheelDiameter * MathConstants::pi) * pulleyRatio / 60_s).value());
 
     m_rightMotor.RestoreFactoryDefaults();
 
@@ -31,8 +31,8 @@ void Shooter::resetMotors(){
     m_rightController.SetD(ksD);
     m_rightController.SetFF(ksFF);
 
-    m_rightEncoder.SetPositionConversionFactor(((wheelDiameter * MathConstants::pi) / pulleyRatio).value());
-    m_rightEncoder.SetVelocityConversionFactor(((wheelDiameter * MathConstants::pi) / pulleyRatio / 60_s).value());
+    m_rightEncoder.SetPositionConversionFactor(((wheelDiameter * MathConstants::pi) * pulleyRatio).value());
+    m_rightEncoder.SetVelocityConversionFactor(((wheelDiameter * MathConstants::pi) * pulleyRatio / 60_s).value());
 
     m_rightMotor.SetIdleMode(CANSparkBase::IdleMode::kCoast);
     m_rightMotor.EnableVoltageCompensation(12.0);
